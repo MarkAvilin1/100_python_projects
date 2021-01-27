192b04feeade34d805e060fb12c7ca1d5d9b7430
MarkAvilin1 <avilin1@outlook.com>
2021-01-27 18:19:46 +0500

    New Update My Blog

--- src/bootstrap-blog/main.py
+++ src/bootstrap-blog/main.py
@@ -11,23 +11,23 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-@app.route('/index.html')
-def home():
-    return render_template("index.html", all_posts=posts)
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
-@app.route("/about.html")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact.html")
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
-
-@app.route("/post.html")
-def post():
-    return render_template("post.html")
 
 
 if __name__ == "__main__":
